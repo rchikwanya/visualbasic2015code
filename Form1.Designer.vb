e637a80652174c9c5b523f0340306fcb41e764c6
rchikwanya <126082592+rchikwanya@users.noreply.github.com>
2023-02-22 16:02:45 +0200

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,344 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class cellphone
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.txtos = New System.Windows.Forms.TextBox()
+        Me.txtdes = New System.Windows.Forms.TextBox()
+        Me.txtsim = New System.Windows.Forms.TextBox()
+        Me.txtcell = New System.Windows.Forms.TextBox()
+        Me.txtmodel = New System.Windows.Forms.TextBox()
+        Me.dgvInfo = New System.Windows.Forms.DataGridView()
+        Me.txtserial = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtID = New System.Windows.Forms.TextBox()
+        Me.txtbrand = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        CType(Me.dgvInfo, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(17, 231)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(41, 13)
+        Me.Label6.TabIndex = 55
+        Me.Label6.Text = "Search"
+        '
+        'txtSearch
+        '
+        Me.txtSearch.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtSearch.BackColor = System.Drawing.SystemColors.Window
+        Me.txtSearch.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSearch.Location = New System.Drawing.Point(106, 224)
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(230, 26)
+        Me.txtSearch.TabIndex = 54
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.SystemColors.Control
+        Me.btnClear.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(548, 178)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(501, 32)
+        Me.btnClear.TabIndex = 53
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+        Me.btnClear.UseVisualStyleBackColor = False
+        '
+        'btnDelete
+        '
+        Me.btnDelete.BackColor = System.Drawing.SystemColors.Control
+        Me.btnDelete.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDelete.Location = New System.Drawing.Point(548, 132)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(501, 32)
+        Me.btnDelete.TabIndex = 52
+        Me.btnDelete.Text = "remove"
+        Me.btnDelete.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+        Me.btnDelete.UseVisualStyleBackColor = False
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.BackColor = System.Drawing.SystemColors.Control
+        Me.btnUpdate.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btnUpdate.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnUpdate.Location = New System.Drawing.Point(548, 96)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(501, 32)
+        Me.btnUpdate.TabIndex = 51
+        Me.btnUpdate.Text = "edit"
+        Me.btnUpdate.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+        Me.btnUpdate.UseVisualStyleBackColor = False
+        '
+        'btnSave
+        '
+        Me.btnSave.BackColor = System.Drawing.SystemColors.Control
+        Me.btnSave.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btnSave.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSave.Location = New System.Drawing.Point(548, 52)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(501, 32)
+        Me.btnSave.TabIndex = 50
+        Me.btnSave.Text = "add"
+        Me.btnSave.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+        Me.btnSave.UseVisualStyleBackColor = False
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(305, 57)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(83, 13)
+        Me.Label5.TabIndex = 46
+        Me.Label5.Text = "Number Of Sims"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(305, 184)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(80, 13)
+        Me.Label7.TabIndex = 49
+        Me.Label7.Text = "Cellphone Date"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(305, 102)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(90, 13)
+        Me.Label8.TabIndex = 47
+        Me.Label8.Text = "Operating System"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(305, 139)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(60, 13)
+        Me.Label9.TabIndex = 48
+        Me.Label9.Text = "Description"
+        '
+        'txtos
+        '
+        Me.txtos.Font = New System.Drawing.Font("Consolas", 12.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtos.Location = New System.Drawing.Point(403, 99)
+        Me.txtos.MaxLength = 64
+        Me.txtos.Name = "txtos"
+        Me.txtos.Size = New System.Drawing.Size(139, 27)
+        Me.txtos.TabIndex = 43
+        '
+        'txtdes
+        '
+        Me.txtdes.Font = New System.Drawing.Font("Consolas", 12.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtdes.Location = New System.Drawing.Point(403, 140)
+        Me.txtdes.MaxLength = 64
+        Me.txtdes.Name = "txtdes"
+        Me.txtdes.Size = New System.Drawing.Size(139, 27)
+        Me.txtdes.TabIndex = 44
+        '
+        'txtsim
+        '
+        Me.txtsim.Font = New System.Drawing.Font("Consolas", 12.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtsim.Location = New System.Drawing.Point(403, 52)
+        Me.txtsim.MaxLength = 16
+        Me.txtsim.Name = "txtsim"
+        Me.txtsim.Size = New System.Drawing.Size(139, 27)
+        Me.txtsim.TabIndex = 42
+        '
+        'txtcell
+        '
+        Me.txtcell.Font = New System.Drawing.Font("Consolas", 12.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtcell.Location = New System.Drawing.Point(403, 181)
+        Me.txtcell.MaxLength = 255
+        Me.txtcell.Name = "txtcell"
+        Me.txtcell.Size = New System.Drawing.Size(139, 27)
+        Me.txtcell.TabIndex = 45
+        '
+        'txtmodel
+        '
+        Me.txtmodel.Font = New System.Drawing.Font("Consolas", 12.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtmodel.Location = New System.Drawing.Point(106, 98)
+        Me.txtmodel.MaxLength = 64
+        Me.txtmodel.Name = "txtmodel"
+        Me.txtmodel.Size = New System.Drawing.Size(139, 27)
+        Me.txtmodel.TabIndex = 34
+        '
+        'dgvInfo
+        '
+        Me.dgvInfo.AllowUserToAddRows = False
+        Me.dgvInfo.AllowUserToDeleteRows = False
+        Me.dgvInfo.AllowUserToResizeRows = False
+        Me.dgvInfo.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.dgvInfo.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.dgvInfo.BackgroundColor = System.Drawing.SystemColors.ControlDark
+        Me.dgvInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvInfo.Location = New System.Drawing.Point(2, 256)
+        Me.dgvInfo.Name = "dgvInfo"
+        Me.dgvInfo.ReadOnly = True
+        Me.dgvInfo.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dgvInfo.Size = New System.Drawing.Size(1063, 369)
+        Me.dgvInfo.TabIndex = 37
+        Me.dgvInfo.TabStop = False
+        '
+        'txtserial
+        '
+        Me.txtserial.Font = New System.Drawing.Font("Consolas", 12.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtserial.Location = New System.Drawing.Point(106, 139)
+        Me.txtserial.MaxLength = 64
+        Me.txtserial.Name = "txtserial"
+        Me.txtserial.Size = New System.Drawing.Size(139, 27)
+        Me.txtserial.TabIndex = 35
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(22, 56)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(68, 13)
+        Me.Label1.TabIndex = 38
+        Me.Label1.Text = "Cellphone ID"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(22, 183)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(35, 13)
+        Me.Label4.TabIndex = 41
+        Me.Label4.Text = "Brand"
+        '
+        'txtID
+        '
+        Me.txtID.Font = New System.Drawing.Font("Consolas", 12.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtID.Location = New System.Drawing.Point(106, 51)
+        Me.txtID.MaxLength = 16
+        Me.txtID.Name = "txtID"
+        Me.txtID.Size = New System.Drawing.Size(139, 27)
+        Me.txtID.TabIndex = 33
+        '
+        'txtbrand
+        '
+        Me.txtbrand.Font = New System.Drawing.Font("Consolas", 12.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtbrand.Location = New System.Drawing.Point(106, 180)
+        Me.txtbrand.MaxLength = 255
+        Me.txtbrand.Name = "txtbrand"
+        Me.txtbrand.Size = New System.Drawing.Size(139, 27)
+        Me.txtbrand.TabIndex = 36
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(22, 101)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(36, 13)
+        Me.Label2.TabIndex = 39
+        Me.Label2.Text = "Model"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(22, 142)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(73, 13)
+        Me.Label3.TabIndex = 40
+        Me.Label3.Text = "Serial Number"
+        '
+        'cellphone
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1067, 622)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtSearch)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.txtos)
+        Me.Controls.Add(Me.txtdes)
+        Me.Controls.Add(Me.txtsim)
+        Me.Controls.Add(Me.txtcell)
+        Me.Controls.Add(Me.txtmodel)
+        Me.Controls.Add(Me.dgvInfo)
+        Me.Controls.Add(Me.txtserial)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtID)
+        Me.Controls.Add(Me.txtbrand)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label3)
+        Me.Name = "cellphone"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "cellphones"
+        CType(Me.dgvInfo, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtSearch As TextBox
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnSave As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents txtos As TextBox
+    Friend WithEvents txtdes As TextBox
+    Friend WithEvents txtsim As TextBox
+    Friend WithEvents txtcell As TextBox
+    Friend WithEvents txtmodel As TextBox
+    Friend WithEvents dgvInfo As DataGridView
+    Friend WithEvents txtserial As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtID As TextBox
+    Friend WithEvents txtbrand As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
